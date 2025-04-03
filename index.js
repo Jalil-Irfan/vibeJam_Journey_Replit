3e534bdf0f1a8666ea9daf58bd36df4dcd47fc14
Jalil Irfan <jalilirfan@gmail.com>
2025-04-03 20:04:05 +0530

    Updates

--- index.js
+++ index.js
@@ -49,8 +49,11 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true
+    outDir: path.resolve(import.meta.dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(import.meta.dirname, "client", "index.html")
+    }
   }
 });
 
